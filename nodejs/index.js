a5045da2e1f5f5bcf49205a96139bc3945500ccc
Navya Nagar <148184506+navyanagar@users.noreply.github.com>
2026-02-12 15:17:52 +0530

    server router

---
+++ nodejs/index.js
@@ -0,0 +1,34 @@
+// const http=require('http');
+
+// const myserver=http.createServer((req,res)=>{
+// console.log("server1")
+// res.end("this is my first server");
+// if(req.url=='/'){
+//     res.end("this is my home page");
+// }
+// else if(req.url=="/about"){
+//     res.end("this is my about page");
+// }
+// else{
+//     res.end("this is contact page");
+// if(req.url === '/') {
+// res.end("This is ABES College");
+// }
+// else if(req.url=="/about")
+// {
+//     res.end(<img src="https://image-static.collegedunia.com/public/reviewPhotos/1159891/IMG_20250611_203218.jpg"></img>)
+// }
+// else{
+// res.end("ABES College is located in Ghaziabad,Uttar Pradesh");
+// }
+// else{
+//     res.end("404 page is not fonund");
+// }
+// })
+// myserver.listen(8000, ()=>console.log("server is run"))
+
+// route- how to make pages connecting to the server
+// if there is no page, it is null route and slash(/) is entry route
+
+
+        
